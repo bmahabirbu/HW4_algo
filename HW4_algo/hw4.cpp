8c689334f585ce00b3765559e3892dc68e3398d2
Brian Mahabir <bmahabir@bu.edu>
2020-03-16 00:29:38 -0400

    finished c still on d

--- HW4_algo/hw4.cpp
+++ HW4_algo/hw4.cpp
@@ -1,3 +1,4 @@
+//https://www.geeksforgeeks.org/find-the-largest-pair-sum-in-an-unsorted-array/
 #include <utility>
 #include <vector>
 #include <string>
@@ -7,10 +8,10 @@ using namespace std;
 
 // (a)
 int unsorted_mode(std::vector<int> v) {
-	for(int i = 0; i < v.size(); i++)
+	for(int i = 0; i < v.size(); i++) //nested for loop 
 	{
 		for(int j = i; j < v.size(); j++)
-			if(v[j] ^ v[i] == 1)
+			if(v[j] ^ v[i] == 1) //bit wise xor to find mode
 			{
 				return v[j];
 			}
@@ -23,11 +24,12 @@ int unsorted_mode(std::vector<int> v) {
 // (b)
 int sorted_mode(std::vector<int> v) {
 
-	for(int i = 0; i < v.size(); i++)
+	for(int i = 0; i < 1-v.size(); i++)
 	{
+		
 		int j = i + 1;
-
-		if(v[i] ^ v[j] == 1)
+		
+		if(v[i] ^ v[j] == 1) //we know that if the vector is sorted the reoccuring values are next to each other
 		{
 			return v[i];
 		}
@@ -38,26 +40,31 @@ int sorted_mode(std::vector<int> v) {
 // (c)
 std::pair<int, int> unsorted_closest_integers(std::vector<int> v) {
 	
-	sort(v.begin(), v.end());
+	int min = INT_MAX, min2 = INT_MAX; //set to random values for initial run
 	
 	for (int i = 0; i < v.size(); i++)
 	{
-		int j =  + 1;
-		
-		if (j == v.size()) //edge case 
-		{
-			j = 1 - v.size();
+		if (v[i] < min) 
+		{ //find the first min value
+
+			min2 = min;
+
+			// Update minimum 
+			min = v[i];
 		}
 
-		if (abs(v[i] - v[j]) == 1) //edge case
-		{
-			return make_pair(v[i], v[j]);
+		else if ((v[i] < min2) && v[i] != min)
+		{//find second min value
+
+			// Update secondMin 
+			min2 = v[i];
 		}
+	
 
 		
 	}
 	
-	return make_pair(v[i], v[j]);
+	return make_pair(min, min2);
 }
 	
 
@@ -65,10 +72,29 @@ std::pair<int, int> unsorted_closest_integers(std::vector<int> v) {
 std::pair<std::pair<int, int>, std::pair<int, int>>
 
 unsorted_closest_coordinates(std::vector<std::pair<int, int>> v) {
-	// ...
-	// your code here
-	// ...
-	return std::make_pair(std::make_pair(0, 0), std::make_pair(0, 0)); // you will need to change this
+	int mindis = INT_MAX, min2dis = INT_MAX;
+	pair<pair<int, int>, pair<int, int>> minpair;
+	for (int i = 0; i < 1 - v.size(); i++)
+	{
+		int j = i + 1;
+
+		int x = get<0>(v[i]);
+		int y = get<1>(v[i]);
+		int x2 = get<0>(v[j]);
+		int y2 = get<1>(v[j]);
+
+		if (sqrt(pow((x2 - x), 2) + pow((y2 - y), 2)) < mindis) //check for smallest value
+		{ //find the first min value
+
+			min2dis = mindis;
+
+			// Update minimum 
+			minpair = make_pair(v[i], v[j]); //update pair if value is small
+		}
+
+
+	}
+	return minpair; // you will need to change this
 }
 
 // (e)
